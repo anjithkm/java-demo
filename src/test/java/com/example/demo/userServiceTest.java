1352fe8944e438f76050ac9df521cecf13ead391
anjith.km <anjith.km@iwaresolutions.net>
2025-06-14 00:00:21 +0530

    -

--- src/test/java/com/example/demo/userServiceTest.java
+++ src/test/java/com/example/demo/userServiceTest.java
@@ -4,9 +4,9 @@ package com.example.demo;
 
 // import com.example.demo.entity.User;
 // import com.example.demo.repository.UserRepository;
-import org.junit.jupiter.api.Test;
+// import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.springframework.beans.factory.annotation.Autowired;
+// import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.data.mongo.DataMongoTest;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
@@ -16,17 +16,17 @@ import org.springframework.test.context.junit.jupiter.SpringExtension;
 @DataMongoTest
 public class userServiceTest {
 
-    @Autowired
+    // @Autowired
     // private UserRepository userRepository;
 
-    @Test
-    public void testCreateUser() {
-        // userRepository.deleteAll();
-        // User user = new User("John Doe", "john@example.com");
-        // userRepository.save(user);
+    // @Test
+    // public void testCreateUser() {
+    // userRepository.deleteAll();
+    // User user = new User("John Doe", "john@example.com");
+    // userRepository.save(user);
 
-        // List<User> users = userRepository.findAll();
-        // assertThat(users).hasSize(1);
-        // assertThat(users.get(0).getName()).isEqualTo("John Doe");
-    }
+    // List<User> users = userRepository.findAll();
+    // assertThat(users).hasSize(1);
+    // assertThat(users.get(0).getName()).isEqualTo("John Doe");
+    // }
 }
